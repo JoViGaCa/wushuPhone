3d14d5250466750ac1ba1d60bd3abc917821d803
JoViGaCa <86113208+JoViGaCa@users.noreply.github.com>
2024-03-01 09:48:13 -0300

    Update bluetoothSocketHandler.java

--- app/src/main/java/com/carvalho/wushuwatchv3/bluetoothSocketHandler.java
+++ app/src/main/java/com/carvalho/wushuwatchv3/bluetoothSocketHandler.java
@@ -1,11 +1,14 @@
 package com.carvalho.wushuwatchv3;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -18,45 +21,42 @@ public class bluetoothSocketHandler  extends Thread{
     Context context;
     BluetoothServerSocket blServerSocket;
     BluetoothSocket blSocket = null;
+    BluetoothServerSocket blServer;
     UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @SuppressLint("MissingPermission")
-    public bluetoothSocketHandler(BluetoothDevice device, Context context){
+    public bluetoothSocketHandler(BluetoothAdapter adapter, Context context){
         this.context = context;
-        this.device = device;
-//        this.blAdapter = blAdapter;
+        this.blAdapter = adapter;
+        BluetoothServerSocket temporary_socket;
 
         // criar socket
         try {
-            blSocket = device.createRfcommSocketToServiceRecord(uuid);
+            temporary_socket = blAdapter.listenUsingInsecureRfcommWithServiceRecord("WhushuPhone",uuid);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-
+        blServer = temporary_socket;
     }
 
     @SuppressLint("MissingPermission")
     public void run() {
         while(true) {
             try {
-                blSocket = device.createRfcommSocketToServiceRecord(uuid);
+                Log.i(TAG,"Starting accept");
+                blSocket = blServer.accept();
             } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                blSocket.connect();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                Log.e(TAG, "Socket's accept() method failed", e);
+                break;
             }
 
-            blHandler = new bluetoothHandler(blSocket,context);
-            blHandler.start();
+            if (blSocket != null) {
+                Log.i(TAG,"Socket created");
 
-            try {
-                Thread.sleep(30000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                blHandler = new bluetoothHandler(blSocket,context);
+                blHandler.start();
+
             }
 
         }
